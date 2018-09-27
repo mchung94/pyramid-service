11f8c7ca04937226522dd63345192be3b35dee92
Mitchell Chung <mchung94@gmail.com>
2018-09-27 12:26:44 -0700

    Make the repositories extend CrudRepository.
    We don't need more functionality than what CrudRepository gives us.

--- src/main/java/com/secondthorn/solitaire/pyramid/service/repository/CardChallengeRepository.java
+++ src/main/java/com/secondthorn/solitaire/pyramid/service/repository/CardChallengeRepository.java
@@ -1,10 +1,10 @@
 package com.secondthorn.solitaire.pyramid.service.repository;
 
 import com.secondthorn.solitaire.pyramid.service.model.CardChallenge;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardChallengeRepository extends JpaRepository<CardChallenge, Long> {
+public interface CardChallengeRepository extends CrudRepository<CardChallenge, Long> {
     CardChallenge findByDeckStringAndGoalRankAndNumToRemove(String deckString, Character goalRank, Integer numToRemove);
 }
--- src/main/java/com/secondthorn/solitaire/pyramid/service/repository/ScoreChallengeRepository.java
+++ src/main/java/com/secondthorn/solitaire/pyramid/service/repository/ScoreChallengeRepository.java
@@ -1,10 +1,10 @@
 package com.secondthorn.solitaire.pyramid.service.repository;
 
 import com.secondthorn.solitaire.pyramid.service.model.ScoreChallenge;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ScoreChallengeRepository extends JpaRepository<ScoreChallenge, Long> {
+public interface ScoreChallengeRepository extends CrudRepository<ScoreChallenge, Long> {
     ScoreChallenge findByDeckStringAndNumPoints(String deckString, Integer numPoints);
 }
